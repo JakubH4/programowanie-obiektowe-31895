4c8acdd1aaba7195e2f1821aa907f4706e68fe73
Jakub Haplicznik <jhaplicznik@edu.cdv.pl>
2025-10-11 16:21:35 +0200

    Zadanie1

--- Lab1/Lab1/Program.cs
+++ Lab1/Lab1/Program.cs
@@ -1,3 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// const int requiredAge = 18;
+// const string accessDenied = "Musisz mieć 18 lat.";
+// const string accessAllowed = "Witamy w naszym sklepie";
+// const string accessPartial = "Możesz przeglądać, ale nie możesz kupować.";
+//
+// int age = 14;
+//
+// do
+// {
+//     Console.WriteLine("Podaj swój wiek: ");
+//     string input = Console.ReadLine();
+//
+//     bool success = int.TryParse(input, out age);
+//
+//     if (!success)
+//     {
+//         Console.WriteLine("Podaj poprawną wartość!");
+//     }
+//     else
+//     {
+//         if (age == 14)
+//         {
+//             Console.WriteLine(accessPartial);
+//         }
+//         else if (age < requiredAge)
+//         {
+//             Console.WriteLine(accessDenied);
+//         }
+//         else
+//         {
+//             Console.WriteLine(accessAllowed);
+//         }
+//     }
+//
+// } while (age < requiredAge);
+//
 
-Console.WriteLine("Hello, World!");
