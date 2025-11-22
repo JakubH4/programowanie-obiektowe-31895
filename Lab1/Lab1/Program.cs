48a2a77e9194e1445f8c21235cf326bf77a7e932
Sochiro <jakubhaplicznik@gmail.com>
2025-11-22 15:51:35 +0100

    zadanie 1

--- Lab1/Lab1/Program.cs
+++ Lab1/Lab1/Program.cs
@@ -1,37 +1,38 @@
-﻿// const int requiredAge = 18;
-// const string accessDenied = "Musisz mieć 18 lat.";
-// const string accessAllowed = "Witamy w naszym sklepie";
-// const string accessPartial = "Możesz przeglądać, ale nie możesz kupować.";
-//
-// int age = 14;
-//
-// do
-// {
-//     Console.WriteLine("Podaj swój wiek: ");
-//     string input = Console.ReadLine();
-//
-//     bool success = int.TryParse(input, out age);
-//
-//     if (!success)
-//     {
-//         Console.WriteLine("Podaj poprawną wartość!");
-//     }
-//     else
-//     {
-//         if (age == 14)
-//         {
-//             Console.WriteLine(accessPartial);
-//         }
-//         else if (age < requiredAge)
-//         {
-//             Console.WriteLine(accessDenied);
-//         }
-//         else
-//         {
-//             Console.WriteLine(accessAllowed);
-//         }
-//     }
-//
-// } while (age < requiredAge);
-//
+﻿// zadanie 1
+const int requiredAge = 18;
+const string accessDenied = "Musisz mieć 18 lat.";
+const string accessAllowed = "Witamy w naszym sklepie";
+const string accessPartial = "Możesz przeglądać, ale nie możesz kupować.";
+
+int age = 14;
+
+do
+{
+    Console.WriteLine("Podaj swój wiek: ");
+    string input = Console.ReadLine();
+
+    bool success = int.TryParse(input, out age);
+
+    if (!success)
+    {
+        Console.WriteLine("Podaj poprawną wartość!");
+    }
+    else
+    {
+        if (age == 14)
+        {
+            Console.WriteLine(accessPartial);
+        }
+        else if (age < requiredAge)
+        {
+            Console.WriteLine(accessDenied);
+        }
+        else
+        {
+            Console.WriteLine(accessAllowed);
+        }
+    }
+
+} while (age < requiredAge);
+
 
